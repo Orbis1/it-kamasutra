d6a2842a820ae3c0115c4a5412f53575bdf7885b
dvkrivosheev <dvkrivosheev@sberbank.ru>
2020-08-10 07:26:17 +0300

    lesson 11

---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Profile = () => {
+  return (
+    <div className="App-content">
+      Content
+      <div>Ava + description</div>
+      <div>
+        My posts
+        <div>New Post</div>
+        <div>
+          Posts
+          <div>Post1</div>
+          <div>Post2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ my-app/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Sidebar = () => {
+  return (
+    <div className="App-sidebar">
+      Sidebar
+      <div><a>Profile</a></div>
+      <div><a>Messages</a></div>
+      <div><a>News</a></div>
+      <div><a>Music</a></div>
+      <div><a>Settings</a></div>
+    </div>
+  )
+}
